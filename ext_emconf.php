0ac0cb2801fe6f8f9140910117883b25e10d2805
Daniel Hoffmann <hoffmann@smply.gd>
2020-02-02 18:14:59 +0100

    [FEATURE] Adds Extension Basicfiles

---
+++ ext_emconf.php
@@ -0,0 +1,29 @@
+<?php
+$EM_CONF[$_EXTKEY] = array (
+	'title' => 's.g. - Rapidmail',
+	'description' => 'Extension for connecting Rapidmail Service to TYPO3',
+	'category' => 'plugin',
+    'author' => 'Daniel Hoffmann',
+    'author_email' => 'hoffmann@smply.gd',
+	'author_company' => 'smply.gd GmbH',
+	'shy' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'version' => '0.0.0',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '9.5.0-9.5.99',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    )
+);
